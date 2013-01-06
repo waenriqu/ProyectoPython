a5e743450bd2c9b9ee8c1d91b4866a50c17754a2
Wilson <wesquall@gmail.com>
2013-01-06 18:25:00 -0500

    Main (Actualizado)
    Main.py actualizado con el codigo de la ruta 1 y algunas correcciones.

--- DemonScar/main.py
+++ DemonScar/main.py
@@ -78,6 +78,78 @@ while (select>2 or select<1):
     else:
         print "Wrong Answer"
 
+pygame.mixer.music.load("C:\Python27\data\PrevioBatalla.mp3")
+pygame.mixer.music.play()
+time.sleep(15)
+#Codigo de Pelea
+
+pygame.mixer.music.load("C:\Python27\data\DespuesBatalla.mp3")
+pygame.mixer.music.play()
+time.sleep(15)
+
+pygame.mixer.music.load("C:\Python27\data\AntesRoom3-2.mp3")
+pygame.mixer.music.play()
+time.sleep(15)
+
+pygame.mixer.music.load("C:\Python27\data\AnteRoom3-3.mp3")
+pygame.mixer.music.play()
+time.sleep(15)
+
+#-----------------------Room 3------------------------------------
+
+print "(1) Camino de la izquierda"
+print "(2) Camino de la derecha"
+
+
+select=0
+while (select>2 or select<1):
+    select=int(raw_input("Your Choice: "))
+    if select==1:
+        pygame.mixer.music.load("C:\Python27\data\Room3op1.mp3")
+        pygame.mixer.music.play()
+        time.sleep(15)
+    elif select==2:
+        ppygame.mixer.music.load("C:\Python27\data\Room3op2.mp3")
+        pygame.mixer.music.play()
+        time.sleep(15)
+    else:
+        print "Wrong Answer"
+
+pygame.mixer.music.load("C:\Python27\data\DespRoom3.mp3")
+pygame.mixer.music.play()
+time.sleep(15)
+
+pygame.mixer.music.load("C:\Python27\data\DespRoom3-2.mp3")
+pygame.mixer.music.play()
+time.sleep(15)
+
+print "(1) Puerta de piedra"
+print "(2) Puerta de huesos"
+print "(3) Puerta de madera"
+
+while (select>3 or select<1):
+    select=int(raw_input("Your Choice: "))
+    if select==1:
+        pygame.mixer.music.load("C:\Python27\data\Room4op1.mp3")
+        pygame.mixer.music.play()
+        time.sleep(15)
+        pygame.mixer.music.load("C:\Python27\data\Room4op1-2.mp3")
+        pygame.mixer.music.play()
+        time.sleep(15)
+        
+    if select==2:
+        ppygame.mixer.music.load("C:\Python27\data\Room4op2.mp3")
+        pygame.mixer.music.play()
+        time.sleep(15)
+    elif select==3:
+        ppygame.mixer.music.load("C:\Python27\data\Room4op3.mp3")
+        pygame.mixer.music.play()
+        time.sleep(15)
+    else:
+        print "Wrong Answer"
+
+
+
 pygame.mixer.music.stop()
 
 
