ad5c185fc7aa25d44c7df6056c0631ffe4d31cba
Pedro Iñiguez <mistery7@hotmail.es>
2013-01-07 10:09:59 -0500

    battle funcion
    batalla funcion

--- DemonScar/heroeclass.py
+++ DemonScar/heroeclass.py
@@ -1,6 +1,11 @@
+# -*- coding: cp1252 -*-
 #Modulo heroeclass.py
 #!\Users\Presario\Documents\python\PyDarkQuest
 
+import sys, pygame
+sys.path.append("C:\Users\Presario\Documents\python\PyDarkQuest")
+from enemie import Enemie
+import random
 
 class Heroe:
     name=""
@@ -36,7 +41,8 @@ class Heroe:
         Heroe.magia=19
 
     def hp_lost(self, atFoe):
-        Heroe.hp=Heroe.hp-(atFoe/defensa)*10
+        print "Damage ", (atFoe/self.defensa)*10
+        Heroe.hp=Heroe.hp-(atFoe/self.defensa)*10
 
     def hp_gain(self, itemRec):
         Heroe.hp=Heroe.hp+itemRec
@@ -49,7 +55,7 @@ class Heroe:
         print "Velocidad:", Heroe.velocidad
         print "Magia:", Heroe.magia
 
-        def indiVida(self):
+    def indiVida(self):
         if self.hp>35:
             print "Estas muy bien"
         elif self.hp>30:
@@ -62,3 +68,40 @@ class Heroe:
             print "Tu vista se nubla, ya no puedes avanzar mas"
         elif self.hp==1:
             print "Deshecho ya ni arrastrarte te servira"
+    def pelea(self, enemigo):
+        while (self.hp>0 and enemigo.hp>0):
+                    if(enemigo.velocidad>self.velocidad):
+                        aleat=random.randrange(1, 5)
+                        print aleat
+                        if (aleat==1):
+                            print "goblin is watching"
+                        else:
+                            print "goblin ataca"
+                            self.hp=self.hp-(enemigo.ataque/self.defensa)*10
+                        print "Atacaras? (1)si (2)no"
+                        select=0
+                        while (select<1 or select>2):
+                            select=int(raw_input("Your Choise: "))
+                        if select==1:
+                           enemigo.hp_lost(self.ataque)
+                        else:
+                            print "No dejes que te engañe"
+                    else:
+                        print "Atacaras? (1)si (2)no"
+                        select=0
+                        while (select<1 or select>2):
+                            select=int(raw_input("Your Choise: "))
+                        if select==1:
+                            enemigo.hp_lost(self.ataque)
+                        else:
+                            print "No dejes que te engañe"
+                        aleat=random.randrange(1, 5)
+                        print aleat
+                        if (aleat==1):
+                            print "goblin is watching"
+                        else:
+                            self.hp=self.hp-(enemigo.ataque/self.defensa)*10
+                            print "goblin ataca"
+                    print self.hp, enemigo.hp
+                    self.indiVida()
+
